a49ee75ae6cd468a1a2422c0bee0c265849a8c66
Abdullah Çanakçı <abdullahcanakci.0@gmail.com>
2020-03-27 12:53:28 +0300

    docker integration

---
+++ build_static.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+parent_folder="$(basename "$PWD")"
+docker run --rm -v $PWD/source:/usr/src/app/source -w /usr/src/app/source -v $PWD/build:/usr/src/app/build docs_app bundle exec middleman build --clean
+
